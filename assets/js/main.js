073af629fe35cc6f3c01e9c6789aa6b9c1a363f3
Guilherme Moreira <guilhermemoreira@geekplug.com.br>
2021-04-01 09:04:49 -0300

    add a initial files

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+const url =
+  "https://spreadsheets.google.com/feeds/cells/1pHdaU7ECJyAuDnDUtWmEbvV3dBi5ZqHiXMVadzhaMAE/od6/public/basic?alt=json";
+
+var request = new XMLHttpRequest();
+
+request.open("GET", url, true);
+request.onload = function () {
+  // Begin accessing JSON data here
+  var { feed } = JSON.parse(this.response);
+
+  if (request.status >= 200 && request.status < 400) {
+    const efetivo = feed.entry[3].content.$t;
+    const vacinados = feed.entry[4].content.$t;
+    const naoVacinado = feed.entry[5].content.$t;
+
+    const date = new Date(feed.updated.$t);
+    let dataFormatada =
+      date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+
+    $(".efetivo").html(efetivo);
+    $(".vacinados").html(vacinados);
+    $(".nao-vacinados").html(naoVacinado);
+    $(".date").html(dataFormatada);
+  } else {
+    console.log("error");
+  }
+};
+
+request.send();
---
+++ assets/js/speedometer.js
@@ -0,0 +1,22 @@
+var amount = 10;
+
+var animatePercentChange = function animatePercentChange(newPercent, elem) {
+  elem = elem || $(".fu-percent span");
+  const val = parseInt(elem.text(), 10);
+
+  if (val !== parseInt(newPercent, 10)) {
+    let diff = newPercent < val ? -1 : 1;
+    elem.text(val + diff);
+    setTimeout(animatePercentChange.bind(null, newPercent, elem), 50);
+  }
+};
+
+$(".fu-progress").on("click", function () {
+  const currentPercent = $(".fu-percent span").text();
+  const waterAnimSpeed = (Math.abs(currentPercent - amount) / 50) * 10;
+  const waterPercent = 100 - amount;
+  animatePercentChange(amount);
+  $(".water").css({
+    top: waterPercent + "%",
+  });
+});
